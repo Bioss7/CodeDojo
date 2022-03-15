bee343bc821e1f09cb93ea0eeb98802c6007af44
bioss7 <fireleny@gmail.com>
2022-03-15 19:41:36 +0700

    Refactoring

--- JavaScript-basics/index.js
+++ JavaScript-basics/index.js
@@ -1,29 +1,24 @@
 
-function random(number) {
-    return Math.ceil(Math.random() * number);
-}
-
-let name = prompt("Привет! Как тебя зовут?");
+let greeting = 'Привет! Как тебя зовут?';
+let name = prompt(greeting);
 
 while (name == '' || name == null) {
-    name = prompt("Привет! Как тебя зовут?");
+    name = prompt(greeting);
 } 
 
 alert(name + ', я загадал число от 1 до 100. Попробуй отгадать его за наименьшее количество попыток. После каждой попытки я скажу "мало", "много" или "угадал"');
 
 let number = random(100);
 let guess = prompt('Какое число я загадал?');
-let numberOfGuessses = 0;
+let numberOfGuessses = 1;
 
-while(guess != number){
-    if(guess > number){
+while(guess != number) {
+    numberOfGuessses++;
+
+    if (guess > number) {
         guess = prompt('Много. Попробуй еще раз');
-        numberOfGuessses++;
-    }
-
-    if(guess < number){
+    } else if(guess < number) {
         guess = prompt('Мало. Попробуй еще раз');
-        numberOfGuessses++;
     }
 }
 
